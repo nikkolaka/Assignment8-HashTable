fb80d27770e53f0ab3845023c87c24809a6df5b8
nikkolaka <nikolaka007@gmail.com>
2022-05-22 15:31:12 -0700

    FINALALALAl

--- src/MyHashTable.java
+++ src/MyHashTable.java
@@ -18,14 +18,7 @@ public class MyHashTable <Key extends Comparable<Key>, Value>{
     private Integer hash(Key key){
         int hash = Math.abs(key.hashCode()) % capacity;
 
-        /*if(key.getClass().getName() != Integer){
 
-        } else{
-            hash = key. % capacity;
-        }*/
-        if(hash < 0){
-            System.out.println("Word: "+key+", Hash: "+hash+", Non-Modded: "+key.hashCode());
-        }
         return hash;
     }
 
@@ -40,22 +33,26 @@ public class MyHashTable <Key extends Comparable<Key>, Value>{
 
         for (int i = hashTemp; i < capacity; i++) {
 
-            tempProbe++;
+
             if(secondPass && i == hashTemp){
+
                 break;
             }
             if(keyBuckets[hashTemp] == null){
+
                 break;
             }
             if(keyBuckets[i] == null){
+
                 break;
             }
-
+            tempProbe++;
             if(keyBuckets[i].compareTo(key) == 0){
 
                 temp = valueBuckets[i];
                 break;
             }
+
 
 
 
@@ -87,7 +84,16 @@ public class MyHashTable <Key extends Comparable<Key>, Value>{
                 break;
             }
 
+
+
             tempProbe++;
+
+            if(keyBuckets[i] != null && keyBuckets[i].compareTo(key) == 0){
+                keyBuckets[i] = key;
+                valueBuckets[i] = value;
+
+                break;
+            }
             comparisons++;
             if(keyBuckets[i] == null){
 
@@ -97,6 +103,8 @@ public class MyHashTable <Key extends Comparable<Key>, Value>{
                 size++;
                 break;
             }
+
+
 
 
 
@@ -125,11 +133,17 @@ public class MyHashTable <Key extends Comparable<Key>, Value>{
         StringBuilder str = new StringBuilder();
 
         for (int i = 0; i < capacity; i++) {
-            if(i == 0){
-                str.append("[").append(keyBuckets[i]).append(":").append(valueBuckets[i]);
-            } else{
-                str.append(", ").append(keyBuckets[i]).append(":").append(valueBuckets[i]);
+
+            if(keyBuckets[i] != null){
+
+                if(i == 0){
+                    str.append("[").append(keyBuckets[i]).append(":").append(valueBuckets[i]);
+                } else{
+                    str.append(", ").append(keyBuckets[i]).append(":").append(valueBuckets[i]);
+                }
+
             }
+
 
         }
         str.append("]");
